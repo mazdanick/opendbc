8b77444db9573df43c2017a5311112d1f22d3390
MazdaNick <nick.tompk@gmail.com>
2025-10-30 18:27:00 -0400

    Slower rate for ACC cancel msg to avoid cruise disable

--- opendbc/car/mazda/carcontroller.py
+++ opendbc/car/mazda/carcontroller.py
@@ -35,9 +35,9 @@ class CarController(CarControllerBase, IntelligentCruiseButtonManagementInterfac
       # will disable the crz 'main on'. crz ctrl msg runs at 50hz. 70ms allows us to
       # read 3 messages and most likely sync state before we attempt cancel.
       self.brake_counter = self.brake_counter + 1
-      if self.frame % 10 == 0 and not (CS.out.brakePressed and self.brake_counter < 7):
+      if self.frame % 20 == 0 and not (CS.out.brakePressed and self.brake_counter < 7):
         # Cancel Stock ACC if it's enabled while OP is disengaged
-        # Send at a rate of 10hz until we sync with stock ACC state
+        # Send at a rate of 5hz until we sync with stock ACC state
         can_sends.append(mazdacan.create_button_cmd(self.packer, self.CP, CS.crz_btns_counter, Buttons.CANCEL))
     else:
       self.brake_counter = 0
