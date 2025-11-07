a4ae18c7d4325bf0d0621950aa0130651e2508b9
MazdaNick <nick.tompk@gmail.com>
2025-11-06 23:25:36 -0500

    5hz. revert brake_counter

--- opendbc/car/mazda/carcontroller.py
+++ opendbc/car/mazda/carcontroller.py
@@ -35,7 +35,7 @@ class CarController(CarControllerBase, IntelligentCruiseButtonManagementInterfac
       # will disable the crz 'main on'. crz ctrl msg runs at 50hz. 70ms allows us to
       # read 3 messages and most likely sync state before we attempt cancel.
       self.brake_counter = self.brake_counter + 1
-      if self.frame % 10 == 0 and not (CS.out.brakePressed and self.brake_counter < 8):
+      if self.frame % 20 == 0 and not (CS.out.brakePressed and self.brake_counter < 7):
         # Cancel Stock ACC if it's enabled while OP is disengaged
         # Send at a rate of 10hz until we sync with stock ACC state
         can_sends.append(mazdacan.create_button_cmd(self.packer, self.CP, CS.crz_btns_counter, Buttons.CANCEL))
@@ -51,9 +51,10 @@ class CarController(CarControllerBase, IntelligentCruiseButtonManagementInterfac
     # send HUD alerts
     if self.frame % 50 == 0:
       ldw = CC.hudControl.visualAlert == VisualAlert.ldw
-      steer_required = CC.hudControl.visualAlert == VisualAlert.steerRequired
+      #steer_required = CC.hudControl.visualAlert == VisualAlert.steerRequired
       # TODO: find a way to silence audible warnings so we can add more hud alerts
-      steer_required = steer_required and CS.lkas_allowed_speed
+      #steer_required = steer_required and CS.lkas_allowed_speed
+      steer_required = CS.out.steerFaultTemporary
       can_sends.append(mazdacan.create_alert_command(self.packer, CS.cam_laneinfo, ldw, steer_required))
 
     # send steering command
